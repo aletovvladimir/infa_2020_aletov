19c596233de732e38f34157d7fbc3eb0b149cf34
aletovvladimir <72245011+aletovvladimir@users.noreply.github.com>
2020-10-02 09:45:14 +0300

    Update 5145.py

--- 5145.py
+++ 5145.py
@@ -21,6 +21,12 @@ screen = pygame.display.set_mode((xsize, ysize))
 screen.fill(gray)
 
 def bkground(screen, color1, color2, y):
+    '''
+    screen - экран отображения
+    color1 - цвет земли
+    color2 - цвет неба
+    y - координата начала земли
+    '''
     rect(screen, color1,(0, y, xsize, ysize))
     rect(screen, color2, (0, 0, xsize, y))
 
@@ -28,6 +34,13 @@ def bkground(screen, color1, color2, y):
 def circles_sheet(screen,
                   x, y, height, R,
                   color_sheet):
+    '''
+    рисует листья дерева
+    x, y - координаты центра круга
+    height - высота дерева
+    R - радиус окружности
+    color_sheet - цвет листьев
+    '''
     y0 = y - height
     circle(screen, color_sheet, (x0, y0 - R), R)
     circle(screen, color_sheet, (x0, y0 + R), R)
@@ -37,6 +50,12 @@ def house(screen,
           x, y,
           width, height,
           color_house, color_roof, color_window):
+    '''
+    рисует дом
+    x, y - координаты середины нижней стороны
+    height, width - полная высота и ширина дома
+    color_house, color_roof, color_window - цвета дома, крыши и окна соответсвенно
+    '''
     x0 = x - int(width / 2)
     y0 = y - int(height / 2)
     wind_width = int(width / 2)
@@ -53,21 +72,30 @@ def tree(screen,
          x, y,
          width, height,
          color_tree, color_sheet):
+    '''
+    рисует дерево
+    x, y - координаты середины нижней стороны ствола
+    width, height - ширина и высота дерева
+    color_tree, color_sheet - цвет ствола и листьев соотвественно
+    '''
     x0 = x - int(width / 2)
     y0 = y - height
     rect(screen, color_tree, (x0, y0, width, height))
     R = 60
-    circle(screen, color_sheet, (x, y0 - R), R)
-    circle(screen, color_sheet, (x - width, y0 - 25), R)
-    circle(screen, color_sheet, (x - width, y0 - 2 * R), R)
-    circle(screen, color_sheet, (x + width, y0 - 25), R)
-    circle(screen, color_sheet, (x + width, y0 - 2 * R), R)
-    circle(screen, color_sheet, (x, y0 - 3 * R), R)
+    xall = (x, x - width, x + width, x - width, x + width, x)
+    yall = (y0 - R, y0- 25, y0 - 25, y0 - 2 * R, y0 - 2 * R)
+    for j, i in xall, yall:
+    circle(screen, color_sheet, (j, i), R)
 
 
 def clouds(screen,
            x, y,
            color_cloud):
+    '''
+    рисует облака
+    x, y - координаты "центра облака"
+    color_cloud - цвет облака
+    '''
     R = 50
     for i in range(0, 10):
         x0 = randint(x - R, x + R)
